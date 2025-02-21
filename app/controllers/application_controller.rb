26a3d93e57834129480542818bccdc78e990564b
prajwal-gadhave-selldo <prajwal.ghadhave@sell.do>
2025-02-21 15:08:14 +0530

    fix: Some changes fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
 
 
   def not_found
-    render file: "#{Rails.root}/public/404.html", layout: false, status: 404
+    render file: "#{Rails.root}/public/404.html", layout: false, status: :not_found
   end
 
 
