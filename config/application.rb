9ff5a6506aa489bf88e9be518ea642843ab73d85
prajwal-gadhave-selldo <prajwal.ghadhave@sell.do>
2025-02-26 18:44:26 +0530

    feat: Implemented Search, Filter and Export Functionality

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,8 @@ module SparkIo
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
+    config.hosts << "e1e9-219-91-158-194.ngrok-free.app"
+    config.hosts << "5327-219-91-158-194.ngrok-free.app"
 
     # Configuration for the application, engines, and railties goes here.
     #
